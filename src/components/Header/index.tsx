c45f586f84e4ed9e6ebfc86aa0e3ebe19feed66d
Sidney Souza <sidney.dev@gmail.com>
2021-06-01 22:09:01 -0400

    wip challenge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
+import styles from './styles.module.scss';
+
 export default function Header(): JSX.Element {
-  return <p>header</p>;
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/images/logo.svg" alt="spacetraveling" />
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-
 // import { getPrismicClient } from '../services/prismic';
 
+import Header from '../components/Header';
+
 // import commonStyles from '../styles/common.module.scss';
-// import styles from './home.module.scss';
+import styles from './home.module.scss';
 
 interface Post {
   uid?: string;
@@ -31,9 +33,83 @@ export default function Home(): JSX.Element {
       <Head>
         <title>welcome | spacetraveling</title>
       </Head>
-      <div>
-        <h1>Welcome</h1>
-      </div>
+
+      <Header />
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div className={styles.info}>
+              <div>
+                <span>📅</span>
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <span>👤</span>
+                <span>Sidney Souza</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div className={styles.info}>
+              <div>
+                <span>📅</span>
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <span>👤</span>
+                <span>Sidney Souza</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div className={styles.info}>
+              <div>
+                <span>📅</span>
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <span>👤</span>
+                <span>Sidney Souza</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div className={styles.info}>
+              <div>
+                <span>📅</span>
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <span>👤</span>
+                <span>Sidney Souza</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div className={styles.info}>
+              <div>
+                <span>📅</span>
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <span>👤</span>
+                <span>Sidney Souza</span>
+              </div>
+            </div>
+          </a>
+        </div>
+      </main>
     </>
   );
 }
